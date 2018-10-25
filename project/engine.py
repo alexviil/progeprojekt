fd9b5606e429586bcdc3a242a0752089b47edcae
Alex <alexviil145@live.com>
2018-10-25 09:02:51 +0300

    fondid ja engine muudatused

--- project/engine.py
+++ project/engine.py
@@ -5,11 +5,11 @@ def main():
     screen_width = 80
     screen_height = 50
 
-    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD) ## font saab välja vahetada, peaks olema fondid kaust
 
-    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+    libtcod.console_init_root(screen_width, screen_height, 'tiitel', False) ## boolean määrab kas fullscreen või mitte
 
-    while not libtcod.console_is_window_closed():
+    while not libtcod.console_is_window_closed(): ## main loop nö
         libtcod.console_set_default_foreground(0, libtcod.white)
         libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
         libtcod.console_flush()
@@ -20,5 +20,5 @@ def main():
             return True
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': ## teeb nii, et main() käivitub ainult siis, kui see engine.py on main script ehk esimesena käima lükatud
      main()
