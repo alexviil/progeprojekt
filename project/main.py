4df9b51bcb0e8c4a15e0388cde1b80b9bc7cad27
Janar Aava <aavajanar@gmail.com>
2018-11-10 23:20:29 +0200

    Eraldasin classi

--- project/main.py
+++ project/main.py
@@ -1,50 +1,46 @@
 import pygame as pg
 import libtcodpy as libt
 import constants as const
-import Actor
-import Draw
-import Map
+import Actor, Draw, Map
 
 
-def game_loop():
-    run = True
-    while run:
-        # Get input
-        events = pg.event.get()
+class Main:
+    def __init__(self):
+        pg.init()
+        self.surface_main = pg.display.set_mode((const.MAIN_SURFACE_HEIGHT, const.MAIN_SURFACE_WIDTH))
 
-        # Process input
-        for event in events:
-            if event.type == pg.QUIT:
-                pg.quit()
-                run = False
+        map_obj = Map.Map()
+        map_obj.create_map()
+        self.game_map = map_obj.get_game_map()
 
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_w:
-                    PLAYER.control(0, -1)
-                elif event.key == pg.K_s:
-                    PLAYER.control(0, 1)
-                elif event.key == pg.K_a:
-                    PLAYER.control(-1, 0)
-                elif event.key == pg.K_d:
-                    PLAYER.control(1, 0)
+        self.player = Actor.Actor(1, 1, const.SPRITE_PLAYER, self.game_map, self.surface_main)
 
-        # Draw game
-        Draw.Draw(SURFACE_MAIN, MAP, PLAYER).draw_game()
+    def game_loop(self):
+        run = True
+        while run:
+            # Get input
+            events = pg.event.get()
 
+            # Process input
+            for event in events:
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    run = False
 
-def game_init():
-    global SURFACE_MAIN, MAP, PLAYER
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_w:
+                        self.player.control(0, -1)
+                    elif event.key == pg.K_s:
+                        self.player.control(0, 1)
+                    elif event.key == pg.K_a:
+                        self.player.control(-1, 0)
+                    elif event.key == pg.K_d:
+                        self.player.control(1, 0)
 
-    pg.init()
-    SURFACE_MAIN = pg.display.set_mode((const.MAIN_SURFACE_HEIGHT, const.MAIN_SURFACE_WIDTH))
-
-    map_obj = Map.Map()
-    map_obj.create_map()
-    MAP = map_obj.get_game_map()
-
-    PLAYER = Actor.Actor(1, 1, const.SPRITE_PLAYER, MAP, SURFACE_MAIN)
+            # Draw game
+            Draw.Draw(self.surface_main, self.game_map, self.player).draw_game()
 
 
 if __name__ == '__main__':
-    game_init()
-    game_loop()
+    game = Main()
+    game.game_loop()
