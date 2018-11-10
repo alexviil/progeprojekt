a3a4c88ec571d0479c08399e358277319a3f621e
Alex <alexviil145@gmail.com>
2018-11-10 22:15:08 +0200

    upscaleitud sprite'id lisatud ning pygame'i scale'id eemaldatud

--- project/constants.py
+++ project/constants.py
@@ -10,9 +10,9 @@ MAIN_SURFACE_WIDTH = 600
 GRAY = (122, 122, 122)
 
 # Sprites
-SPRITE_PLAYER = pg.transform.scale2x(pg.image.load("sprites/wizzard_m_idle_anim_f0.png"))
-SPRITE_WALL = pg.transform.scale2x(pg.image.load("sprites/wall_mid.png"))
-SPRITE_FLOOR = pg.transform.scale2x(pg.image.load("sprites/floor_1.png"))
+SPRITE_PLAYER = pg.image.load("sprites/wizzard_m_idle_anim_f0.png")
+SPRITE_WALL = pg.image.load("sprites/wall_mid.png")
+SPRITE_FLOOR = pg.image.load("sprites/floor_1.png")
 
 # Map values
 MAP_WIDTH = 10
